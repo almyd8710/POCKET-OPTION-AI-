6d44ef6ffb335b994616086bb709edc2d532c745
almyd8710 <almyd8710@gmail.com>
2025-07-11 22:52:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -211,7 +211,7 @@ async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(ADMIN_ID, f"❌ خطأ:\n{tb[:4000]}")
 
 # تشغيل البوت
-app = ApplicationBuilder().token("7728605631:AAFPOZ8ni818s5LspoEUyXH2jrTApK9J_9s").build()
+app = ApplicationBuilder().token("7795034366:AAFsJ0tAhzcLZVYB0nNf7TFmHUTKd80eNi4").build()
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("change", change_pairs))
